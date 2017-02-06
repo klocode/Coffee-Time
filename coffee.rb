d4d3364e40caa8308068da17e1054c75283d20c7
klocode <kendrick.t.lo@gmail.com>
2017-02-06 15:10:36 -0500

    finished explorer, initial tests

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,25 @@
 class Coffee
-  
+  attr_accessor :name, :amount
+
+  def initialize(name, amount=3)
+    @name = name
+    @amount = amount
+  end
+
+  def full?
+    if amount == 3
+      true
+    else
+      false
+    end
+  end
+
+  def empty?
+    if amount == 0
+      true
+    else
+      false
+    end
+  end
+
 end
--- human.rb
+++ human.rb
@@ -1,10 +1,32 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :coffee
 
   def initialize(name, alertness=0)
     self.name = name
     self.alertness = alertness
+  end
+
+  def has_coffee?
+    coffee
+  end
+
+  def needs_coffee?
+    if coffee
+      false
+    else
+      true
+    end
+  end
+
+  def buy(coffee)
+    @coffee = coffee
+  end
+
+  def drink!
+    coffee.amount -= 1
+    self.alertness += 0.31
   end
 
 
